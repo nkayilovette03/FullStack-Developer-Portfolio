1c87c429671543725d8ceb7be6feeb6ca319c8a2
nkayilovette03 <nkayilovette03@gmail.com>
2023-11-16 11:22:42 -0500

    Push public changes

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,83 +1,83 @@
-import React from 'react'
-import Image from 'next/image'
+import Image from "next/image";
+import React from "react";
 
-import img1 from '../public/1.jpg'
-import img2 from '../public/2.jpg'
-import img3 from '../public/3.jpg'
-import img4 from '../public/4.jpg'
-import img5 from '../public/5.jpg'
-import img6 from '../public/6.jpg'
-import img7 from '../public/7.jpg'
-import img8 from '../public/8.jpg'
-import img9 from '../public/9.jpg'
-import img10 from '../public/10.jpg'
-import img11 from '../public/11.jpeg'
+import img1 from "../public/1.jpg";
+import img10 from "../public/10.jpg";
+import img11 from "../public/11.jpeg";
+import img2 from "../public/2.jpg";
+import img3 from "../public/3.jpg";
+import img4 from "../public/4.JPG";
+import img5 from "../public/5.jpg";
+import img6 from "../public/6.jpg";
+import img7 from "../public/7.jpg";
+import img8 from "../public/8.jpg";
+import img9 from "../public/9.jpg";
 
 const DATA = [
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img1,
-    title: 'Life is just what you believe it is, suit yourself',
+    title: "Life is just what you believe it is, suit yourself",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img2,
-    title: 'Brother From Another Mother',
+    title: "Brother From Another Mother",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img3,
-    title: 'Another Shot From Brother',
+    title: "Another Shot From Brother",
   },
 
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img4,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img5,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img6,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img7,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img8,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img9,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img10,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
   {
     id: Math.floor(Math.random() * 1000),
     imgUrl: img11,
-    title: 'CryptoKing On The Flow',
+    title: "CryptoKing On The Flow",
   },
-]
+];
 
 const Gallery = () => {
   return (
     <>
       {/* <div className=''> */}
       {DATA.map((item, index) => (
-        <div key={item.id} className='col-lg-3 gallery-image'>
+        <div key={item.id} className="col-lg-3 gallery-image">
           <Image
             src={item.imgUrl}
             alt={item.title}
@@ -85,7 +85,7 @@ const Gallery = () => {
             // objectFit='cover'
             // width={2400}
             // height={1598}
-            layout='fill'
+            layout="fill"
             quality={100}
             priority
           />
@@ -93,7 +93,7 @@ const Gallery = () => {
       ))}
       {/* </div> */}
     </>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
